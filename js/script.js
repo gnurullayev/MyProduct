05236b29c28ed30c749803403867bb9bd2dd9323
gnurullayev <nurullayevgayrat73@gmail.com>
2022-07-22 00:24:35 +0500

    script file change

--- js/script.js
+++ js/script.js
@@ -1,53 +1,45 @@
-let body = document.querySelector("body"),
-    nav = document.querySelector(".nav"),
-    menu = document.querySelector(".menu"),
-    menuClouse = document.querySelector(".menu-clouse");
+  let body = document.querySelector("body"),
+      nav = document.querySelector(".nav"),
+      menu = document.querySelector(".menu"),
+      menuClouse = document.querySelector(".menu-clouse");
 
-menu.addEventListener("click", () => {
-    nav.style.left = "0" 
-    body.style.overflow = "hidden"
-})
-
-menuClouse.addEventListener("click", () => {
-  nav.style.left = "-100%" 
-  body.style.overflow = "scroll"
-
-})
-
-let changeColor = document.querySelector(".button-back"),
-    header = document.querySelector(".header"),
-    navLink = document.querySelectorAll(".nav__link"),
-    logoText = document.querySelectorAll(".logo__text"),
-    footerNavLink = document.querySelectorAll(".footer__nav-link"),
-    product = document.querySelectorAll(".product")
-    console.log(product)
-
-changeColor.addEventListener("click", () => {
-  body.classList.toggle("body2");
-  header.classList.toggle("header2");
-
-
-  logoText.forEach(logoText => {
-    logoText.style.color = "#fff"
+  menu.addEventListener("click", () => {
+      nav.style.left = "0" 
+      body.style.overflow = "hidden"
   })
 
+  menuClouse.addEventListener("click", () => {
+    nav.style.left = "-100%" 
+    body.style.overflow = "scroll"
 
-  navLink.forEach(navLink => {
-    navLink.style.color = "rgba(255, 255, 255, 0.8)"
+  })
 
-    navLink.addEventListener("mouseover", () => {
-      navLink.style.color = "#fff"
+  let changeColor = document.querySelector(".button-back"),
+      header = document.querySelector(".header"),
+      navLink = document.querySelectorAll(".nav__link"),
+      logoText = document.querySelectorAll(".logo__text"),
+      footerNavLink = document.querySelectorAll(".footer__nav-link"),
+      product = document.querySelector(".product"),
+      process = document.querySelector(".process")
+
+  changeColor.addEventListener("click", () => {
+    body.classList.toggle("body2");
+    header.classList.toggle("header2");
+    product.classList.toggle("product2");
+    process.classList.toggle("process2");
+
+
+    logoText.forEach(logoText => {
+      logoText.classList.toggle("logo__text2")
     })
 
-    navLink.addEventListener("mouseout", () => {
-      navLink.style.color = "rgba(255, 255, 255, 0.8)"
+
+    navLink.forEach(navLink => {
+      navLink.classList.toggle("nav__link2")
     })
+
+
+    footerNavLink.forEach(footerNavLink => {
+      footerNavLink.style.color = "#fff"
   })
-
-
-  footerNavLink.forEach(footerNavLink => {
-    footerNavLink.style.color = "#fff"
-  })
-
-
 })
